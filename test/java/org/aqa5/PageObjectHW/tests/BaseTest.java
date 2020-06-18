a7604af5c56d20befd213fdbd4cff0ca697345fb
alexander-trostyanko-simpligov <atrostyanko@simpligov.com>
2020-06-18 19:48:52 +0300

    Init BaseTest.java

---
+++ test/java/org/aqa5/PageObjectHW/tests/BaseTest.java
@@ -0,0 +1,12 @@
+package java.org.aqa5.PageObjectHW.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+    WebDriver driver;
+
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+}
